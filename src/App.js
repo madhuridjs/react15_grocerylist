287cb4fbfcd76b9e329cf8910e99d46d8961c563
madhuri <madhuri.djs3@gmail.com>
2021-04-18 18:00:37 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+    return(
+        <section className= "section-center">
+            <div className= "grocery-container">
+                <h3>List</h3>
+                <button className= "clear-btn">clear all items</button>
+            </div>
+        </section>
+        
+    )
+}
+export default App;
